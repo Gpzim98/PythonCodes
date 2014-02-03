55b1f2f3605cecd4b56776a1439a5772eb20ea46
gpzim98 <gpzim98>
2014-02-03 00:21:13 -0200

    Alter folder name

---
+++ CursoPythonUnderLinux/Aula001/2HelloWorldInterective.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+name = raw_input('Digite seu nome:')
+print 'Hello ', name
---
+++ CursoPythonUnderLinux/Aula001/4HelloWorldOutraFormaCodificacao.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+frase = raw_input('Digite uma frase com caracter especial:')
+print frase
