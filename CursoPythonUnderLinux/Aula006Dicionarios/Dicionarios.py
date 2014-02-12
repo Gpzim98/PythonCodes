9b7de72c7c2b89f413773c9abae38a9c542d3eb5
gpzim98 <gpzim98>
2014-02-12 21:13:13 -0200

    Dicictionaries and variables

---
+++ CursoPythonUnderLinux/Aula006Dicionarios/Dicionarios.py
@@ -0,0 +1,10 @@
+dic  = {1:'Gregory', 2:'Susy'}
+dic2 = {'Um':1, 2:'Dois'}
+dic3 = {1:dic2} #Um dicionario dentro do outro
+dic4 = dict(teste=1, teste2=2) #Argumentos
+dic5 = dict([(1,2), ('Gpzim98', 'Susy Ane')]) #Lista de tuplas
+dic6 = dict(((1,'Lista item 1'), (2,'Lista item 2'))) # Tupla de tuplas
+dic7 = dict([[1, 'Item lista 1'], [2, 'Item lista 2']]) # Lista de listas
+
+print dic, dic2, dic3, dic4, dic5, dic6, dic7
+
